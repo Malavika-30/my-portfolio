69afd5d248c52454ebd5139a0571f2e1e46f5d4b
Malavika-30 <rmalavika6@gmail.com>
2026-08-04 17:44:45 +0530

    NEW

--- script.js
+++ script.js
@@ -5,3 +5,73 @@ menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
+
+// ===============================
+// 3D CARD TILT EFFECT
+// ===============================
+
+
+const cards=document.querySelectorAll(
+".skill-box,.experience-card,.education-content,.achievement-card,.project-card"
+);
+
+
+
+cards.forEach(card=>{
+
+
+card.addEventListener("mousemove",(e)=>{
+
+
+const rect=card.getBoundingClientRect();
+
+
+const x=e.clientX-rect.left;
+
+const y=e.clientY-rect.top;
+
+
+const centerX=rect.width/2;
+
+const centerY=rect.height/2;
+
+
+const rotateX=
+((y-centerY)/centerY)*10;
+
+
+const rotateY=
+((x-centerX)/centerX)*10;
+
+
+
+card.style.transform=
+`
+perspective(900px)
+rotateX(${-rotateX}deg)
+rotateY(${rotateY}deg)
+translateY(-10px)
+`;
+
+
+
+});
+
+
+
+card.addEventListener("mouseleave",()=>{
+
+
+card.style.transform=
+`
+perspective(900px)
+rotateX(0)
+rotateY(0)
+translateY(0)
+`;
+
+
+});
+
+
+});
